ab038630543c54ea433f399d9653ae8fd6b75eaf
minar <mina.roger84@gmail.com>
2020-09-23 16:55:28 +0200

    innit

--- emailer/emailer.py
+++ emailer/emailer.py
@@ -1,12 +1,14 @@
 from django.core.mail import send_mail
 from mytut.settings import EMAIL_HOST_USER
+from celery import shared_task
 
 
+@shared_task
 def send_welcome_email(email, name):
     send_mail(
         'Welcome!' + name,
         'Here is the message.',
         EMAIL_HOST_USER,
         [email],
-        fail_silently=True,
+        fail_silently=False
     )
